c8147c5aef86bc5916b3333fc4064753a51317e3
Anatoliy Korsakov <tolik.kaz91@gmail.com>
2016-07-28 00:13:22 +0600

    Properties Options

--- src/main/java/io/hibernate/AppConfig.java
+++ src/main/java/io/hibernate/AppConfig.java
@@ -5,19 +5,52 @@ import io.hibernate.dao.ContactTelDetail;
 import io.hibernate.dao.Hobby;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableConfigurationProperties
 @EnableTransactionManagement
 @ComponentScan(basePackages = "io.hibernate")
+@PropertySource("classpath:application.properties")
+@ImportResource("app-context.xml")
 public class AppConfig {
+
+    private
+    @Value("${username}")
+    String username;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private PropertyClass propertyClass;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+
+        Resource[] resources = new ClassPathResource[]{
+                new ClassPathResource("application.properties")};
+
+        pspc.setLocations(resources);
+        pspc.setIgnoreResourceNotFound(true);
+        pspc.setIgnoreUnresolvablePlaceholders(true);
+        pspc.setLocalOverride(true);
+        return pspc;
+    }
 
     @Bean
     public SessionFactory sessionFactory() {
@@ -27,13 +60,29 @@ public class AppConfig {
                 .addAnnotatedClasses(Hobby.class)
                 .buildSessionFactory();
     }
+
+    @PostConstruct
+    public void printProperties() {
+        SpringPropertiesUtil.printProperty(
+                "env.getProperty(\"username\")",
+                env.getProperty("username"));
+        SpringPropertiesUtil.printProperty(
+                "SpringPropertiesUtil.getProperty(\"username\")",
+                SpringPropertiesUtil.getProperty("username"));
+        SpringPropertiesUtil.printProperty(
+                "@Value(\"${username}\"", username);
+        SpringPropertiesUtil.printProperty(
+                "propertyClass.getToken()", propertyClass.getToken());
+    }
+
     @Bean
     public DataSource getDataSource() {
+
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/dev_hibernate");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/" + env.getProperty("mysql.database"));
         dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setPassword(env.getProperty("mysql.password"));
 
         return dataSource;
     }
